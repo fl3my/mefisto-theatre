6d151cfcf9b97093f8fa2be080b87a824563eb32
Ross Fleming <rossflem@icloud.com>
2023-01-18 15:48:51 +0000

    Add PersonalData annotation to User model.

--- MefistoTheatre/Models/ApplicationUser.cs
+++ MefistoTheatre/Models/ApplicationUser.cs
@@ -4,7 +4,11 @@ namespace MefistoTheatre.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        // PersonalData tag allows the user to retrieve the data on the frontend.
+        [PersonalData]
         public string? FirstName { get; set; }
+
+        [PersonalData]
         public string? LastName { get; set; }
     }
 }
