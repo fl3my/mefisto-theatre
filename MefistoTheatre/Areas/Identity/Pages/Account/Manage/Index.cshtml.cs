05e0cae77cff9c171c69b96650a1361f598a580d
Ross Fleming <rossflem@icloud.com>
2023-01-23 21:13:55 +0000

    Add extra properties to the manage personal details form.

--- MefistoTheatre/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ MefistoTheatre/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using MefistoTheatre.Models;
+using MefistoTheatre.Validators;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -56,12 +57,26 @@ namespace MefistoTheatre.Areas.Identity.Pages.Account.Manage
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-
+            [Required]
             [Display(Name = "First Name")]
             public string FirstName { get; set; }
 
+            [Required]
             [Display(Name = "Last Name")]
             public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Address")]
+            public string Address { get; set; }
+
+            [Required]
+            [Display(Name = "City")]
+            public string City { get; set; }
+
+            [Required]
+            [RegularExpression("^([Gg][Ii][Rr] 0[Aa]{2}|([A-Za-z][0-9]{1,2}|[A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2}|[A-Za-z][0-9][A-Za-z]|[A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]) {0,1}[0-9][A-Za-z]{2})$", ErrorMessage = "Invalid Postcode.")]
+            [Display(Name = "PostCode")]
+            public string PostCode { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
@@ -74,7 +89,10 @@ namespace MefistoTheatre.Areas.Identity.Pages.Account.Manage
             Input = new InputModel
             {
                 FirstName = user.FirstName,
-                LastName = user.LastName
+                LastName = user.LastName,
+                Address = user.Address,
+                City = user.City,
+                PostCode = user.PostCode,
             };
         }
 
@@ -107,6 +125,9 @@ namespace MefistoTheatre.Areas.Identity.Pages.Account.Manage
             // Update the custom properties.
             var firstName = user.FirstName;
             var lastName = user.LastName;
+            var address = user.Address;
+            var city = user.City;
+            var postCode = user.PostCode;
 
             if (Input.FirstName != firstName)
             {
@@ -117,6 +138,24 @@ namespace MefistoTheatre.Areas.Identity.Pages.Account.Manage
             if (Input.LastName != lastName)
             {
                 user.LastName = Input.LastName;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.Address != address)
+            {
+                user.Address = Input.Address;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.City != city)
+            {
+                user.City = Input.City;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.PostCode != postCode)
+            {
+                user.PostCode = Input.PostCode;
                 await _userManager.UpdateAsync(user);
             }
 
